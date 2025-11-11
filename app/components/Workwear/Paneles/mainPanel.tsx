2248f1556c557ccbfba1030325c96a0a67ba6246
ENNA-Egor <e.yudakov@gmail.com>
2025-11-11 19:24:15 +0300

    Добавил инпутов

--- app/components/Workwear/Paneles/mainPanel.tsx
+++ app/components/Workwear/Paneles/mainPanel.tsx
@@ -4,7 +4,10 @@ export const MainPanel =() => {
     return (
         <div>
             <h3>Главный</h3>
-            <input type="Text"></input>
+            <input type="Text" placeholder="Фамилия"></input>
+            <input type="Text" placeholder="Имя"></input>
+            <input type="Text" placeholder="Отчество"></input>
+            <input type="Text" placeholder="Компания"></input>
         </div>
     )
 }
