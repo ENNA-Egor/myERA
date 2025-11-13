c1ab4c3f6fb9e05026a15857ee4694e15ebff729
ENNA-Egor <e.yudakov@gmail.com>
2025-11-13 08:52:37 +0300

    Заготовка под блок основного окна

--- app/components/Workwear/Paneles/MainPanel/MainPanel.tsx
+++ app/components/Workwear/Paneles/MainPanel/MainPanel.tsx
@@ -1,6 +1,20 @@
+import React, {useState} from 'react'
 import { UserData} from './User'
 
+const userValue = {
+    users:[
+        {id: 1, surname:'Иванов', first_name: 'Владимир', patronymic: 'Петрович'},
+        {id: 2, surname:'Петров', first_name: 'Иван', patronymic: 'Сидорович'},
+        {id: 3, surname:'Сидоров', first_name: 'Юрий', patronymic: 'Викторович'},
+        {id: 4, surname:'Бобров', first_name: 'Семён', patronymic: 'Николаевич'},
+        {id: 5, surname:'Мишин', first_name: 'Василий', patronymic: 'Олегович'},
+        {id: 6, surname:'Володин', first_name: 'Сидор', patronymic: 'Ефимович'}
+    ]
+}
+
 export const MainPanel =() => {
+
+    const [user, setUser] = useState(userValue);
 
     return (
         <div>
