24c26d3acd57e90acee6a95a069cf6cf5c44e952
ENNA-Egor <e.yudakov@gmail.com>
2025-11-19 11:16:44 +0300

    Настроил очистку формы при отправке из  PersonalCard

--- app/components/Workwear/Paneles/PersonalCard.tsx
+++ app/components/Workwear/Paneles/PersonalCard.tsx
@@ -1,61 +1,100 @@
 
-import React, {useState} from "react";
-import { useDispatch} from 'react-redux'
-import {addUser } from '../../../Store/Slice/UserSlice'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../../../Store/Slice/UserSlice";
 
+export const PersonalCard = () => {
+  const initialNewUsers = {
+    surname: "",
+    first_name: "",
+    patronymic: "",
+    profession: "",
+  };
 
-export const PersonalCard =() => {
+  const [user, setUser] = useState(initialNewUsers);
+  const dispatch = useDispatch();
 
-    const initialNewUsers ={
-       surname: '',
-       first_name: '',
-       patronymic: '', 
-       profession: '',
-    }
-    
-    const [user, setUser] = useState(initialNewUsers);
-    
-    const handleChange = (event) => {
-        setUser ({
-            ...user,
-            [event.target.name]: (event.target.value)
-         })
-    }
+  const handleChange = (event) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    const dispatch = useDispatch();
+  const handleSubmit = async () => {
+    // если кнопка внутри <form>, то нужно event.preventDefault()
+    // event.preventDefault();
 
-    const handleSubmit = (event) => {
-        dispatch(addUser(user))
-    }
+    // если dispatch асинхронный (thunk) и вы хотите ждать результата:
+    // await dispatch(addUser(user));
 
-    return (
-        <div className="persCard">
-            <h3>Новая карточка</h3>
-            <div className="persCardBlok">
-                <label className="persCardData">
-                    Фамилия
-                <input type="Text" className="inputCard" name='surname' onChange={handleChange}></input>
-                </label>
-            </div>
-            <div className="persCardBlok">
-                <label className="persCardData">
-                Имя
-                <input type="Text" className="inputCard" name='first_name' onChange={handleChange}></input>
-                </label>
-            </div>
-            <div className="persCardBlok">
-                <label className="persCardData">
-                    Отчество
-                <input type="Text" className="inputCard" name='patronymic' onChange={handleChange}></input>
-                </label>
-            </div>
-            <div className="persCardBlok">
-                <label className="persCardData">
-                    Профессия
-                <input type="Text" className="inputCard" name='profession' onChange={handleChange}></input>
-                </label>
-            </div>
-            <button className="personInput btnMenu allBtn"  onClick={handleSubmit}>Add</button>
-        </div>
-    )
-}
+    dispatch(addUser(user)); // отправляем
+    setUser(initialNewUsers); // очищаем форму
+  };
+
+  return (
+    <div className="persCard">
+      <h3>Новая карточка</h3>
+
+      <div className="persCardBlok">
+        <label className="persCardData">
+          Фамилия
+          <input
+            type="text"
+            className="inputCard"
+            name="surname"
+            value={user.surname}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <div className="persCardBlok">
+        <label className="persCardData">
+          Имя
+          <input
+            type="text"
+            className="inputCard"
+            name="first_name"
+            value={user.first_name}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <div className="persCardBlok">
+        <label className="persCardData">
+          Отчество
+          <input
+            type="text"
+            className="inputCard"
+            name="patronymic"
+            value={user.patronymic}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <div className="persCardBlok">
+        <label className="persCardData">
+          Профессия
+          <input
+            type="text"
+            className="inputCard"
+            name="profession"
+            value={user.profession}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+
+      <button
+        type="button" // безопаснее, если нет <form>
+        className="personInput btnMenu allBtn"
+        onClick={handleSubmit}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
