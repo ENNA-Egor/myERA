74dee8ed043cada40e962a5b20a65c091b3b79cb
ENNA-Egor <e.yudakov@gmail.com>
2025-11-21 09:41:53 +0300

    Отредактировал превстейт

--- app/Store/Slice/WorkwearSlice.ts
+++ app/Store/Slice/WorkwearSlice.ts
@@ -52,13 +52,16 @@ interface valueUser {
 //     ] },
 // ]
 const valueUser =  [
-    { id: 11, idUser:1, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: ''},
-    { id: 12, idUser:2, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: ''},
-    { id: 13, idUser:3, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: '' },
-    { id: 14, idUser:4, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: '' },
-    { id: 15, idUser:5, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: '' },
-    { id: 16, idUser:6, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: '' },
-    { id: 17, idUser:7, typeWorkswear:'Костюм зимний',date_issue: '', date_dismissal: '', prise: 0, residual_prise: 0, size: '', height: '' },
+    { id: 11, idUser:1, typeWorkswear:'Костюм зимний',date_issue: '10.11.2025', date_dismissal: '', prise: 2500, residual_prise: 0, size: '48-50', height: '182-188'},
+    { id: 18, idUser:1, typeWorkswear:'Костюм летний',date_issue: '10.09.2025', date_dismissal: '', prise: 2500, residual_prise: 0, size: '48-50', height: '182-188'},
+    { id: 12, idUser:2, typeWorkswear:'Костюм летний',date_issue: '10.11.2025', date_dismissal: '', prise: 1000, residual_prise: 0, size: '48-50', height: '182-188'},
+    { id: 19, idUser:2, typeWorkswear:'Костюм зимний',date_issue: '20.11.2025', date_dismissal: '', prise: 1000, residual_prise: 0, size: '48-50', height: '182-188'},
+    { id: 13, idUser:3, typeWorkswear:'Костюм зимний',date_issue: '10.11.2025', date_dismissal: '', prise: 2500, residual_prise: 0, size: '48-50', height: '182-188' },
+    { id: 14, idUser:4, typeWorkswear:'Костюм зимний',date_issue: '10.11.2025', date_dismissal: '', prise: 2500, residual_prise: 0, size: '48-50', height: '182-188' },
+    { id: 15, idUser:5, typeWorkswear:'Костюм летний',date_issue: '10.11.2025', date_dismissal: '', prise: 1000, residual_prise: 0, size: '48-50', height: '182-188' },
+    { id: 16, idUser:6, typeWorkswear:'Костюм зимний',date_issue: '10.11.2025', date_dismissal: '', prise: 2500, residual_prise: 0, size: '48-50', height: '182-188' },
+    { id: 17, idUser:7, typeWorkswear:'Костюм летний',date_issue: '10.11.2025', date_dismissal: '', prise: 1000, residual_prise: 0, size: '48-50', height: '182-188' },
+    { id: 20, idUser:7, typeWorkswear:'Костюм зимний',date_issue: '14.11.2025', date_dismissal: '', prise: 1000, residual_prise: 0, size: '48-50', height: '182-188' },
 ]
 
 
