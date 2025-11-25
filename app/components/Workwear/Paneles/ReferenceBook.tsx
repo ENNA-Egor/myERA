fceb84880816d24af3155c94480ef558aa8a720a
ENNA-Egor <e.yudakov@gmail.com>
2025-11-25 15:43:38 +0300

    Добавил кнопку "Редактировать"

--- app/components/Workwear/Paneles/ReferenceBook.tsx
+++ app/components/Workwear/Paneles/ReferenceBook.tsx
@@ -1,10 +1,17 @@
 import React from "react"
+
+import myImage from '../../../assets/edit-button-svgrepo-com.svg';
+
+
 export const ReferenceBook =() => {
 
     return (
         <div>
             <h3>Справочники</h3>
             <input type="Text"></input>
+            <button>
+                 <img src={myImage}  className="imgButton"/>
+            </button>
         </div>
     )
 }
