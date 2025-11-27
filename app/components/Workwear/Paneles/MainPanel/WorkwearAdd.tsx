076f59d011a018fb900bba7d414b8ecf56d40c37
ENNA-Egor <e.yudakov@gmail.com>
2025-11-27 14:10:07 +0300

    Закрытие карточки. Подстроилстили

--- app/components/Workwear/Paneles/MainPanel/WorkwearAdd.tsx
+++ app/components/Workwear/Paneles/MainPanel/WorkwearAdd.tsx
@@ -1,12 +1,23 @@
+import { useDispatch} from 'react-redux'
+import {toggleWorkswear } from '../../../../Store/Slice/ModalWindowSlice'
+
+
 export const WorkwearAdd =() => {
+    const dispatch = useDispatch();
+
+    const handleClickWorkswear =()=> {
+               dispatch(toggleWorkswear(1))
+         }
 
     return (
         <div>
             <h3>Главный</h3>
-            <input type="Text" placeholder="Фамилия"></input>
-            <input type="Text" placeholder="Имя"></input>
-            <input type="Text" placeholder="Отчество"></input>
-            <input type="Text" placeholder="Компания"></input>
+            <input type="Text" placeholder="Тип СИЗ"></input>
+            <input type="Text" placeholder="Период носки"></input>
+            <input type="Text" placeholder="Стоимость"></input>
+            <input type="Text" placeholder="Рзмер"></input>
+            <input type="Text" placeholder="Рост"></input>
+            <button onClick={handleClickWorkswear}>Close</button>
         </div>
     )
 }
