2d6ec0bd6e188bac83c9ec88681a5922703a3c92
ENNA-Egor <e.yudakov@gmail.com>
2025-12-08 20:47:33 +0300

    1234

--- app/Store/Slice/UserSlice.ts
+++ app/Store/Slice/UserSlice.ts
@@ -12,7 +12,7 @@ interface valueUser {
 }
 
 const valueUser =  [
-    { id: 1, surname: 'Fert', first_name: 'Voljnd', patronymic: 'Dfrgy', profession:'arb'},
+    { id: 1, surname: 'Fert', first_name: 'Voljnd', patronymic: 'Dfrgy', profession:'arb', status: false, dateOut:''},
     { id: 2, surname: 'Petr', first_name: 'Jhon', patronymic: 'Send' , profession:'Vod', status: false, dateOut:''},
     { id: 3, surname: 'Singh', first_name: 'Evgen', patronymic: 'Virtor' , profession:'Tract', status: true, dateOut:''},
     { id: 4, surname: 'Bobr', first_name: 'Dod', patronymic: 'Michail', profession:'Tract', status: false , dateOut:''},
